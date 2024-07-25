ec12a6b36c0993f416a1ce39ef4fc3c479b06df9
Edilson Schwanck <edilsonborges96@gmail.com>
2024-07-25 10:51:40 -0300

    Initial Commit

---
+++ CargasBrasil/CargasBrasilApp.swift
@@ -0,0 +1,17 @@
+//
+//  CargasBrasilApp.swift
+//  CargasBrasil
+//
+//  Created by Edilson Borges on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CargasBrasilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
